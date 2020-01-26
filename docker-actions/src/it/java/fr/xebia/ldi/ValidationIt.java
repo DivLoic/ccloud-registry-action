527ca79aa9cbaf00bad85e031258f52f62ccbb63
DivLoic <ldivad@isep.fr>
2020-01-26 15:00:04 +0100

    add the It classes, resources and executions

---
+++ docker-actions/src/it/java/fr/xebia/ldi/ValidationIt.java
@@ -0,0 +1,16 @@
+package fr.xebia.ldi;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+/**
+ * Created by loicmdivad.
+ */
+public class ValidationIt {
+
+    @Test
+    public void testShouldFail() {
+        fail();
+    }
+}
